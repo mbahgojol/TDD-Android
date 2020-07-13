7a5856b3fdb680f3332f80451c9b9bb064d244eb
ghozimahdi <profghozimahdi@gmail.com>
2020-07-13 12:52:21 +0700

    remove main viewmodel

--- app/src/main/java/com/blank/chapter10/ui/main/MainActivity.kt
+++ app/src/main/java/com/blank/chapter10/ui/main/MainActivity.kt
@@ -2,28 +2,12 @@ package com.blank.chapter10.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.blank.chapter10.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val viewmodel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewmodel.getCount().observe(this, Observer {
-            tvCount.text = it.toString()
-        })
-
-        btnTambah.setOnClickListener {
-            viewmodel.increment()
-        }
-
-        btnKurang.setOnClickListener {
-            viewmodel.decrement()
-        }
     }
 }
